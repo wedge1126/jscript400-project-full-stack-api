717c51e1487a700d6f7ea895e013dd7145ce9c7f
Jonathan Veilleux <wedge1126@gmail.com>
2019-07-23 00:53:13 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const { MONGO_DB_CONNECTION, NODE_ENV, PORT } = process.env
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+
+// Database Connection
+if (MONGO_DB_CONNECTION) {
+  mongoose.connect(MONGO_DB_CONNECTION, { useNewUrlParser: true, useFindAndModify: false })
+  console.log('Connected to database...')
+} else {
+  console.log('Could not connect to database!')
+}
+
+// Application-level Middleware
+if (NODE_ENV === 'development') app.use(require('morgan')('dev'))
+app.use(require('body-parser').json())
+
+// Routes
+
+// Not Found Handler
+app.use((req, res, next) => {
+  const error = new Error(`Could not ${req.method} ${req.path}`)
+  error.status = 404
+  next(error)
+})
+
+// Error Handler
+app.use((err, req, res, next) => {
+  if (NODE_ENV === 'development') console.error(err)
+  const { message, status } = err
+  res.status(status).json({ status, message })
+})
+
+// Open Connection
+const listener = () => console.log(`Listening on Port ${PORT}!`)
+app.listen(PORT, listener)
