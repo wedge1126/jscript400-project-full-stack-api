ac9b2b47a19906865fa90ee801abbc790b03c133
Jonathan Veilleux <wedge1126@gmail.com>
2019-07-23 04:01:23 -0700

    implemented more API routes

--- api/routes/units.js
+++ api/routes/units.js
@@ -21,8 +21,10 @@ router.get('/', async (req, res, next) => {
   let query = Object.assign({}, req.query)
   delete query.occupied
 
-  if(occupied === 'true' || occupied === 'false') {
-    query.company = { $exists: occupied === 'true' }
+  if(occupied === 'true') {
+    query.company = { $exists: true, $ne: null }
+  } else if(occupied === 'false') {
+    query.company = null
   }
 
   const response = await Units.find(query).select('-__v')
@@ -61,6 +63,9 @@ router.patch('/:id/company', async (req, res, next) => {
       next({ status: 404, message: 'Unit not found'})
     }
 
+    if(!unit.company) {
+      unit.company = {}
+    }
     Object.assign(unit.company, req.body)
     const { _id, kind, floor, special_monthly_offer, created_at, updated_at, company } = await unit.save()
     const response = { _id, kind, floor, special_monthly_offer, created_at, updated_at, company }
@@ -71,6 +76,60 @@ router.patch('/:id/company', async (req, res, next) => {
     } else {
       next({ status: 500, message: err })
     }
+  }
+})
+
+router.delete('/:id/company', async (req, res, next) => {
+  const status = 200
+  const { id } = req.params
+
+  try {
+    const unit = await Units.findById(id).select('-__v')
+    if(unit === null) {
+      next({ status: 404, message: 'Unit not found'})
+    }
+
+    unit.company.remove()
+    const response = await unit.save()
+    res.json({ status, response })
+  } catch (err) {
+    if(err.name === 'ValidationError') {
+      next({ status: 400, message: err })
+    } else {
+      next({ status: 500, message: err })
+    }
+  }
+})
+
+router.get('/:id/company/employees', async (req, res, next) => {
+  const status = 200
+  const { id } = req.params
+
+  const unit = await Units.findById(id).select('-__v')
+  if(unit === null) {
+    next({ status: 404, message: 'Unit not found'})
+  } else if(!unit.company) {
+    next({ status: 404, message: 'Company not found. Unit is unoccupied.'})
+  }
+
+  const response = unit.company.employees
+  res.json({ status, response })
+})
+
+router.get('/:id/company/employees/:empId', async (req, res, next) => {
+  const status = 200
+  const { id, empId } = req.params
+
+  const unit = await Units.findById(id).select('-__v')
+  if(unit === null) {
+    next({ status: 404, message: 'Unit not found'})
+  } else if(!unit.company) {
+    next({ status: 404, message: 'Company not found. Unit is unoccupied.'})
+  } else if(!unit.company.employees.id(empId)) {
+    next({ status: 404, message: 'Employee not found.'})
+  } else {
+    const response = unit.company.employees.id(empId)
+    res.json({ status, response })
   }
 })
 
